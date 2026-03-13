4298747592f829590939780458cb4c7f33905389
malay77patra <malay77patra@gmail.com>
2026-03-13 23:58:26 +0530

    feat: initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="w-full max-w-3xl mx-auto px-6">
+        <nav className='h-16 flex items-center justify-between'>
+            ~/qubyalgo
+        </nav>
+    </div>
+  )
+}
